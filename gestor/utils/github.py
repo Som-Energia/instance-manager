0fb8b351e7acbe6a440753fee260ab78ae36e015
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-09 17:48:01 +0100

    Add logging and custom exceptions

--- gestor/utils/github.py
+++ gestor/utils/github.py
@@ -1,21 +1,34 @@
+import logging
 from typing import Any
 
 import aiohttp
 
 from gestor.schemas.git import GitInfo
 
+_logger = logging.getLogger(__name__)
+
+
+class InvalidGitHubUrl(Exception):
+    pass
+
 
 async def _github_request(path: str) -> Any:
     url = f"https://api.github.com{path}"
 
+    _logger.info("Fetching GitHub API (%s)", url)
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 404:
-                raise Exception("GitHub URL not found")
+                raise InvalidGitHubUrl("GitHub URL not found (%s)" % url)
             return await response.json()
 
 
-async def get_info_from_pull_request(repository: str, pull_request: int):
+async def get_pull_request_info(repository: str, pull_request: int):
+    _logger.info(
+        "Getting pull request information from GitHub API (%s/%d)",
+        repository,
+        pull_request,
+    )
     path = f"/repos/{repository}/pulls/{pull_request}"
     pull_request_info = await _github_request(path)
     return GitInfo(
