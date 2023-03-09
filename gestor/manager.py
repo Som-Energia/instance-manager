10d28d2791a6fd0996dcad3c4fa53bff40c30f3d
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-09 17:48:49 +0100

    Add logic to create instance from a pull request

--- gestor/manager.py
+++ gestor/manager.py
@@ -1,19 +1,58 @@
-from gestor.schemas.instance import InstanceCreate
-from gestor.utils.github import get_info_from_pull_request
+import logging
+
+from config import settings
+from gestor.models.git import GitInfoModel
+from gestor.models.instance import InstanceModel
+from gestor.schemas.instance import Instance
+from gestor.utils import github, kubernetes
+from gestor.utils.database import SessionLocal
+
+_logger = logging.getLogger(__name__)
+
+
+async def start_instance(instance: Instance) -> None:
+    _logger.info("Starting instance (%s)", str(instance.dict()))
+    data = {
+        "name": instance.name,
+        "domain": settings.DEPLOY_DOMAIN,
+        "labels": {},
+        **instance.git_info.dict(),
+    }
+    try:
+        await kubernetes.new_deployment(instance.name, data)
+    except Exception as e:
+        _logger.error("Failed to start the instance:%s", str(e))
+        return
 
 
 class Manager:
-    async def deploy(self, instance: InstanceCreate) -> None:
-        pass
+    db = SessionLocal()
 
-    async def deploy_pull_request(self, repository: str, pull_request: int) -> None:
-        commit = await get_commit_from_pull_request(repository, pull_request)
-        instance = InstanceCreate(
-            commit=commit,
-            repository=repository,
-            pull_request=pull_request,
+    @classmethod
+    async def start_pr_instance(cls, repository: str, pull_request: int) -> None:
+        try:
+            git_info = await github.get_pull_request_info(repository, pull_request)
+        except github.InvalidGitHubUrl as e:
+            _logger.error("Error getting pull request information:%s" % str(e))
+            return
+
+        # Allow just one instance for a repository pull_request
+        instance = GitInfoModel.get_git_info_instance(cls.db, git_info)
+        if instance:
+            # TODO: Check for new commits and update
+            _logger.error(
+                "An instance for %s/%d already exists:%s",
+                repository,
+                pull_request,
+                instance.name,
+            )
+            return
+
+        # Create and start a new instance
+        instance = Instance.from_orm(
+            InstanceModel.create_instance(cls.db, Instance(git_info=git_info))
         )
-        await self.deploy(instance)
+        await start_instance(instance)
 
 
 manager = Manager()
