b991c15f757f0b4b9fb22356c86f43017aef3f3e
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-10 16:32:15 +0100

    Start manager on application startup

--- gestor/main.py
+++ gestor/main.py
@@ -3,11 +3,12 @@ import logging
 import uvicorn
 from fastapi import FastAPI
 
+from gestor import manager
 from gestor.routers import api, webhooks
 
 logging.basicConfig(level=logging.DEBUG)
-app = FastAPI(title="Gestor", description="Gestor")
 
+app = FastAPI(title="Gestor", description="Gestor")
 app.include_router(api.router, prefix="/api")
 app.include_router(webhooks.router, prefix="/webhooks")
 
@@ -15,6 +16,11 @@ app.include_router(webhooks.router, prefix="/webhooks")
 @app.get("/")
 async def root():
     return {"msg": "Hello World"}
+
+
+@app.on_event("startup")
+async def startup_event():
+    await manager.manager.start()
 
 
 if __name__ == "__main__":
