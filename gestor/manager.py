ee16ac3ffd7c4c104b59bdbd158a5933d5dd561c
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-12 02:55:36 +0100

    Maintain db through k8s events

--- gestor/manager.py
+++ gestor/manager.py
@@ -1,5 +1,5 @@
-import asyncio
 import logging
+from asyncio import create_task, Queue
 
 from kubernetes.client import V1Deployment
 
@@ -16,7 +16,7 @@ _logger = logging.getLogger(__name__)
 class Manager:
     def __init__(self):
         self._db = SessionLocal()
-        self._loop_tasks = []
+        self._tasks = []
 
     async def start_instance_from_pull_request(
         self, repository: str, pull_request: int
@@ -30,7 +30,6 @@ class Manager:
         # Allow just one instance for a repository pull_request
         instance = GitInfoModel.get_git_info_instance(self._db, git_info)
         if instance:
-            # TODO: Check for new commits and update
             _logger.error(
                 "An instance for %s/%d already exists:%s",
                 repository,
@@ -38,11 +37,7 @@ class Manager:
                 instance.name,
             )
             return
-
-        # Create and start a new instance
-        instance = Instance.from_orm(
-            InstanceModel.create_instance(self._db, Instance(git_info=git_info))
-        )
+        instance = Instance(git_info=git_info)
         await instance.deploy()
 
     async def init_db_from_cluster(self):
@@ -50,9 +45,31 @@ class Manager:
         for deployment in deployments:
             instance = Instance.parse_obj(await self.deployment_to_dict(deployment))
             InstanceModel.create_instance(self._db, instance)
+            InstanceModel.create_instance(self._db, instance)
+
+    async def watch_kubernetes_events(self, event_queue):
+        await kubernetes.watch_deployments(event_queue)
+
+    async def process_kubernetes_events(self, event_queue):
+        while True:
+            event = await event_queue.get()
+            event_type = event["type"]
+            deployment = event["object"]
+            _logger.debug("Event %s %s", event_type, deployment.metadata.name)
+            instance = Instance.parse_obj(await self.deployment_to_dict(deployment))
+            try:
+                if event_type == "ADDED":
+                    InstanceModel.create_instance(self._db, instance)
+                elif event_type == "DELETED":
+                    InstanceModel.delete_instance(self._db, instance)
+            except Exception:
+                pass
 
     async def start(self) -> None:
-        asyncio.create_task(self.init_db_from_cluster())
+        event_queue = Queue()
+        self._tasks.append(create_task(self.init_db_from_cluster()))
+        self._tasks.append(create_task(self.watch_kubernetes_events(event_queue)))
+        self._tasks.append(create_task(self.process_kubernetes_events(event_queue)))
 
     @staticmethod
     async def deployment_to_dict(deployment: V1Deployment):
