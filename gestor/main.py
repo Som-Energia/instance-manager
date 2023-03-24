efeb7179d74d3ad669306a58bb4b1ddc63a69f04
Xavier Terés <xavier.teres@somenergia.coop>
2023-03-24 11:45:53 +0100

    Add CORS config

--- gestor/main.py
+++ gestor/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from gestor import manager
 from gestor.routers import api, webhooks
@@ -10,6 +11,18 @@ logging.basicConfig(level=logging.DEBUG)
 app = FastAPI(title="Gestor", description="Gestor")
 app.include_router(api.router, prefix="/api")
 app.include_router(webhooks.router, prefix="/webhooks")
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
